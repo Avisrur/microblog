fad285add980e5f5883c47aaa462d9a1160d83a4
Avisrur <nadavavisrur@gmail.com>
2020-06-02 13:11:05 +0300

    added update and delete

---
+++ src/main/java/microblog/dto/PostUpdate.java
@@ -0,0 +1,14 @@
+package microblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PostUpdate {
+    private String body;
+}
