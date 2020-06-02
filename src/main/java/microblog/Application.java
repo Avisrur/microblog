d3afa971ab213ff19a3700f864efdcfec2b35d35
Avisrur <nadavavisrur@gmail.com>
2020-06-02 12:37:06 +0300

    added tests infra

--- src/main/java/microblog/Application.java
+++ src/main/java/microblog/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication()
-//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@EnableAutoConfiguration()
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
