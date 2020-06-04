6a2987c349da575c3dac01589731cbcab3436316
Avisrur <nadavavisrur@gmail.com>
2020-06-04 12:04:57 +0300

    fixed docker-compose

--- src/test/groovy/ct/microblog/PostsSpec.groovy
+++ src/test/groovy/ct/microblog/PostsSpec.groovy
@@ -12,29 +12,12 @@ class PostsSpec extends Specification {
     @Shared
     AppDriver app = new AppDriver()
 
-    @Shared
-    MongoDriver mongoDriver = new MongoDriver()
-
-    def userId = "userId"
-
     def setupSpec() {
         app.start()
     }
 
     def cleanupSpec() {
         app.stop()
-    }
-
-    def "create new post"() {
-        given:
-        def newPost = [userId: userId,
-                       body  : "Hello microblog"]
-
-        when:
-        def response = app.createNewPost(newPost)
-
-        then:
-        assert response.status == 201
     }
 
 
